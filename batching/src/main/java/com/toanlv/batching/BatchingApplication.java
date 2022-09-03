d1bd4ab519029c030104b3f0c11d92c4b5410881
Toan Le <toan.le@ataccama.com>
2022-09-03 21:33:04 +0700

    Learning: update Batch request demo

--- batching/src/main/java/com/toanlv/batching/BatchingApplication.java
+++ batching/src/main/java/com/toanlv/batching/BatchingApplication.java
@@ -1,11 +1,14 @@
 package com.toanlv.batching;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -22,32 +25,40 @@ public class BatchingApplication {
 
 
 @Controller
-class BatchController{
+class BatchController {
 
     @QueryMapping
-    public Flux<Customer> customers(){
+    public Flux<Customer> customers() {
         return Flux.fromIterable(this.customerList);
     }
 
-    @SchemaMapping(typeName = "Customer")
-    Account account(Customer customer){
-        return new Account(customer.getId());
+    @BatchMapping
+    Map<Customer, Account> account(List<Customer> customers) {
+        System.out.println("Calling account from customer");
+        return customers.stream().collect(Collectors.toMap(customer -> customer,
+                customer -> new Account(customer.getId())));
     }
 
-   private List<Customer> customerList = List.of(
-            new Customer(1,"A"),
-            new Customer(2,"B")
-            );
+/* BatchMapping is the same the below code
+    @SchemaMapping(typeName = "Customer")
+    Account account(Customer customer) {
+        return new Account(customer.getId());
+    }
+*/
+
+    private List<Customer> customerList = List.of(new Customer(1, "A"), new Customer(2, "B"));
 }
+
 
 @AllArgsConstructor
 @Getter
-class Customer{
+class Customer {
     private Integer id;
     private String name;
 }
 
+
 @AllArgsConstructor
-class Account{
+class Account {
     private Integer id;
 }
